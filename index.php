3668f82148082407840f8628960dcbfb45312045
firdaus aminnulloh <firdaus_23510027@stimata.ac.id>
2023-11-16 21:00:34 +0700

    Menambahkan operasi aritmatika pada PHP

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+    $x = 10;
+    $y = 20;
+    $jumlah = $x + $y;
+    echo "Hasil Penjumlahan: " . $jumlah . "<br>";
+
+    // Operator Pengurangan
+    $kurang = $x - $y;
+    echo "Hasil Pengurangan: " . $kurang . "<br>";
+
+    // Operator Perkalian
+    $kali = $x * $y;
+    echo "Hasil Perkalian: " . $kali . "<br>";
+
+    // Operator Pembagian
+    $bagi = $x / $y;
+    echo "Hasil Pembagian: " . $bagi . "<br>";
+
+    // Operator Modulus
+    $modulus = $x % $y;
+    echo "Hasil Modulus: " . $modulus . "<br>";
+
+?>
+
